9f5cc13b974355a80e6261a91cbaed7293ffb159
Ryan Munro (Monbrey) <rsm999@uowmail.edu.au>
2019-03-18 16:44:50 +1100

    Fixed arg switching for mart

--- commands/mart/index.js
+++ commands/mart/index.js
@@ -12,27 +12,37 @@ module.exports = class MartCommand extends BaseCommand {
             category: "Game",
             description: "Browse with the URPG Pokemart",
             args: {
-                "itemType": "String"
+                itemType: "String"
             },
             syntax: "!mart <itemType>",
             enabled: true,
-            defaultConfig: { "guild": false }
+            defaultConfig: { guild: false }
         })
     }
 
     async welcome(message) {
         let tb = "```"
         let embed = new RichEmbed()
-            .setTitle(`URPG Pokemart | Balance: $${message.trainer.cash.toLocaleString()}`)
-            .setDescription("The following commands can be used to browse the Pokemart and make purchases")
-            .addField("Catalogue", `${tb}
+            .setTitle(
+                `URPG Pokemart | Balance: $${message.trainer.cash.toLocaleString()}`
+            )
+            .setDescription(
+                "The following commands can be used to browse the Pokemart and make purchases"
+            )
+            .addField(
+                "Catalogue",
+                `${tb}
 Pokemon:  !mart pokemon [page#]
 Items:    !mart items [category]
-Moves:    Unlocked per-Pokemon, see below${tb}`)
-            .addField("Purchases", `${tb}
+Moves:    Unlocked per-Pokemon, see below${tb}`
+            )
+            .addField(
+                "Purchases",
+                `${tb}
 Pokemon:  !buy pokemon [<Pokemon>, <Pokemon>...]
 Items:    !buy items [<Item>, <Item>...]
-Moves:    !buy moves <roster-index>${tb}`)
+Moves:    !buy moves <roster-index>${tb}`
+            )
 
         return message.channel.send(embed)
     }
@@ -42,20 +52,30 @@ Moves:    !buy moves <roster-index>${tb}`)
             message.trainer = await Trainer.findById(message.author.id)
         } catch (e) {
             message.client.logger.parseError(e, this.name)
-            return message.channel.sendPopup("error", "Error fetching Trainer from dataase")
+            return message.channel.sendPopup(
+                "error",
+                "Error fetching Trainer from dataase"
+            )
         }
 
-        switch (args[0]) {
+        const type = args.get("itemType")
+        if (!type) return this.welcome(message)
+
+        switch (type) {
             case "pokemon":
-                if (args[1]) return browsePokemon(message, args[1].match(/[1-9][0-9]*/) ? args[1] : 1)
+                if (args[1])
+                    return browsePokemon(
+                        message,
+                        args[1].match(/[1-9][0-9]*/) ? args[1] : 1
+                    )
                 else return browsePokemon(message)
             case "items":
                 return browseItems(message)
-                // case "em":
-                //    if (args[1]) return await this.tmInterface(message, args[1].match(/[1-9][0-9]*/) ? args[1] : 1)
-                //    else return await this.tmInterface(message)
-                // case "hm":
-                //    return await this.hmInterface(message)
+            // case "em":
+            //    if (args[1]) return await this.tmInterface(message, args[1].match(/[1-9][0-9]*/) ? args[1] : 1)
+            //    else return await this.tmInterface(message)
+            // case "hm":
+            //    return await this.hmInterface(message)
             default:
                 return this.welcome(message)
         }
